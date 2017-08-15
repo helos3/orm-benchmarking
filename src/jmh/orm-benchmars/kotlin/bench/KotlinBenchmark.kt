c275a3468a4d135ddd928ba22fcbc29bcc7e0701
Rushan Piakshev <rpiakshev@berlogic.de>
2017-08-15 16:36:15 +0200

    init commit

---
+++ src/jmh/orm-benchmars/kotlin/bench/KotlinBenchmark.kt
@@ -0,0 +1,21 @@
+package bench
+
+import org.openjdk.jmh.annotations.*
+
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 0)
+@Measurement(iterations = 1)
+open class KotlinBenchmark {
+    var value: Double = 0.0
+
+    @Setup
+    fun setUp(): Unit {
+
+        value = 3.0
+    }
+
+    @Benchmark
+    fun sqrtBenchmark(): Double = Math.sqrt(value)
+
+}
